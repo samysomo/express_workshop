afe02697bf47bbeb41e10f4ea6069289a5bd4a98
samysomo <117149856+samysomo@users.noreply.github.com>
2023-09-24 14:57:05 -0600

    feat: Updated pokemon post route, it now inserts a valid pokemon to the database, returns an error message if fields are not complete

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -3,21 +3,35 @@ const pokemon = express.Router();
 //Base de datos de pokemmon
 const db = require("../config/database");
 
-pokemon.post("/", (req, res, next) => {
-    return res.status(200).json(req.body);
+pokemon.post("/", async (req, res, next) => {
+    const {pok_name, pok_height, pok_weight, pok_base_experience} = req.body;
+
+
+    if (pok_name && pok_height && pok_weight && pok_base_experience){
+        const query = `INSERT INTO pokemon (pok_name, pok_height, pok_weight, pok_base_experience) VALUES('${pok_name}', '${pok_height}', '${pok_weight}', '${pok_base_experience}')`;
+
+        const rows = await db.query(query);
+
+        if (rows.affectedRows == 1){
+            return res.status(201).json({code: 201, message: "Pokemon insertado correctamente"});
+        }
+
+        return res.status(500).json({code: 500, message: "Ocurrió un error"});
+    }
+    return res.status(500).json({code: 500, message:"Campos incompletos"});
 });
 
 //Todos los pokemon
 pokemon.get("/", async (req, res, next)=>{
     const pkmn = await db.query("SELECT * FROM pokemon");
-    return res.status(200).json({code: 1, message: pkmn});
+    return res.status(200).json({code: 200, message: pkmn});
 });
 
 pokemon.get("/:id([0-9]{1,3})", async (req, res, next)=>{
     const id = req.params.id;
     const pkmn = await db.query(`SELECT * FROM pokemon WHERE pok_id = ${id}`);
     if (pkmn.length > 0) {
-        return res.status(200).json({code: 1, message: pkmn});
+        return res.status(200).json({code: 200, message: pkmn});
     } else {
         return res.status(404).json({code: 404, message: "Pokemon no encontrado"});
     }
@@ -29,7 +43,7 @@ pokemon.get("/:name([A-Za-z]+)", async (req, res, next) =>{
     console.log(name);
     const pkmn = await db.query(`SELECT * FROM pokemon WHERE pok_name = '${name}'`);
     if (pkmn.length > 0) {
-        return res.status(200).json({code: 1, message: pkmn});;
+        return res.status(200).json({code: 200, message: pkmn});;
     } else {
         return res.status(404).json({code: 404, message: "Pokemon no encontrado"});
     }
